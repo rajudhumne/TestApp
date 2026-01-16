15f9cbf05036bdbabb6702bc8663e60666cec414
rajudhumne <rajudhumne@gmail.com>
2026-01-16 17:42:49 +0000

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Raju Dhumne on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by Raju Dhumne on 16/01/26.
+//
+
+import Testing
+@testable import TestApp
+
+struct TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
